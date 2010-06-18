e6fe39688b394907ca25e827d013680d9f3dd4c2
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2010-06-18 17:40:45 +0000

    simple three segment connector. does not take other objects into account.
    --HG--
    extra : convert_revision : svn%3A7e9b7e63-0183-aa40-b454-855736b2bbf9/trunk%404

--- pathing/display.h
+++ pathing/display.h
@@ -1,3 +1,26 @@
+vector<vec2> calculateConnector(vec2 e1, vec2 e2, vec2 direction)
+{
+	vector<vec2> vertices;
+	vec2 diff = 0.5*(e2-e1);
+	vec2 x = diff.projection(direction);
+	vec2 y = diff-x;
+
+	vertices.push_back(e1);
+	if(x.norm() > y.norm())	// the direction of inequality can be passed as a template parameter
+	{
+		vertices.push_back(e1+y);
+		vertices.push_back(e2-y);
+	}
+	else
+	{
+		vertices.push_back(e1+x);
+		vertices.push_back(e2-x);
+	} // end else
+	vertices.push_back(e2);
+
+	return vertices;
+} // end function calculateConnector
+
 void display()
 {
 	glStencilMask(0xffffffff);
@@ -10,11 +33,15 @@ void display()
 	} // end for
 	glStencilMask(0);
 
+	vector<vec2> vertices = calculateConnector(handles[0]->position,handles[1]->position,arrows[0]->direction());
+
+	// render connector
 	glColor(blue);
-	glBegin(GL_LINES);
-		glVertex2fv(handles[0]->position);
-		glVertex2fv(handles[1]->position);
+	glBegin(GL_LINE_STRIP);
+		for(uint c = 0 ; c < vertices.size() ; ++c)
+			glVertex2fv(vertices[c]);
 	glEnd();
+	vertices.clear();	// clear connector
 
 	glutSwapBuffers();
 } // end function display
